437e0c508c158a5d7923dfad7934453c0fdaa9fe
Johan Back <johan.backs@gmail.com>
2020-03-24 16:01:28 -0300

    alt backend

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require ("express");
+const mongoose = require("mongoose");
+const cors = require("cors")
+const routes = require("./routes")
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://Semana10:semana10@cluster0-dumev.mongodb.net/test?retryWrites=true&w=majority" , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+app.use(cors());
+app.use(routes);
+app.listen(3333);
